b545d58e3dde804922b652328ad402a94c5a4093
dfang5 <spiritlessentity@gmail.com>
2025-07-30 21:51:04 +1000

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { serve } from "https://deno.land/std@0.224.0/http/server.js";
+import { serve } from "https://deno.land/std@0.224.0/http/server.ts";
 
 // Dummy HTTP server to keep Deno Deploy happy
 serve(() => new Response("Fang Bot is alive!"));
